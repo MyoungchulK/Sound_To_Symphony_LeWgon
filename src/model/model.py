477aff496d86ca124e4f8b3bb3996dda9f640dcb
Myoungchul Kim <kmc8907@gmail.com>
2024-03-17 15:36:06 +0900

    mid saving

--- src/model/model.py
+++ src/model/model.py
@@ -1,25 +1,35 @@
 import tensorflow as tf
 
 class MyModel:
-    def __init__(self, input_shape, seq_length, learning_rate=0.005):
+    def __init__(self, input_shape, seq_length = 25, learning_rate=0.005):
+        """ initializer """
+
+        ## set basic self
         self.input_shape = input_shape
         self.seq_length = seq_length
         self.learning_rate = learning_rate
 
-    def build_model(self):
-        inputs = tf.keras.Input(self.input_shape)
-        x = tf.keras.layers.LSTM(128)(inputs)
+    def build_model(self, ne_size = 128):
+        """ model """
 
+        ## input
+        inputs = tf.keras.Input(self.input_shape)
+        x = tf.keras.layers.LSTM(ne_size)(inputs)
+
+        ## output
         outputs = {
-            'pitch': tf.keras.layers.Dense(128, name='pitch')(x),
+            'pitch': tf.keras.layers.Dense(ne_size, name='pitch')(x),
             'step': tf.keras.layers.Dense(1, name='step')(x),
             'duration': tf.keras.layers.Dense(1, name='duration')(x),
         }
 
+        ## build
         model = tf.keras.Model(inputs, outputs)
         return model
 
     def compile_model(self, model):
+        """ compiler """
+        
         loss = {
             'pitch': tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
             'step': mse_with_positive_pressure,
@@ -28,7 +38,13 @@ class MyModel:
 
         optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
 
-        model.compile(loss=loss, optimizer=optimizer)
+        model.compile(loss=loss, 
+                    optimizer=optimizer,
+                    loss_weights={
+                    'pitch': 0.05,
+                    'step': 1.0,
+                    'duration':1.0,
+                    })
         return model
 
     def get_summary(self, model):
--- src/preproccesing/preprocessing.py
+++ src/preproccesing/preprocessing.py
@@ -3,13 +3,17 @@ import numpy as np
 import collections
 import pretty_midi
 import glob
+import tensorflow as tf
 
 def midi_to_notes(midi_file: str) -> pd.DataFrame:
+    """ midi to note conversion """
+
+    ## extract basic
     pm = pretty_midi.PrettyMIDI(midi_file)
     instrument = pm.instruments[0]
     notes = collections.defaultdict(list)
 
-    # Sort the notes by start time
+    ## Sort the notes by start time
     sorted_notes = sorted(instrument.notes, key=lambda note: note.start)
     prev_start = sorted_notes[0].start
 
@@ -31,19 +35,27 @@ def notes_to_midi(
     instrument_name: str,
     velocity: int = 100,  # note loudness
     ) -> pretty_midi.PrettyMIDI:
+    """ converts the generated note to midi """ 
 
+    ## set basic info for midi
     pm = pretty_midi.PrettyMIDI()
     instrument = pretty_midi.Instrument(
         program=pretty_midi.instrument_name_to_program(
             instrument_name))
 
+    ## conversion
     prev_start = 0
     for i, note in notes.iterrows():
+        ## conditioning to not over the range
+        pitch = max(0, min(127, int(note['pitch'])))
+        velocity = max(0, min(127, velocity))
+
+        ## store in midi
         start = float(prev_start + note['step'])
         end = float(start + note['duration'])
         note = pretty_midi.Note(
             velocity=velocity,
-            pitch=int(note['pitch']),
+            pitch=pitch,
             start=start,
             end=end,
         )
@@ -53,6 +65,34 @@ def notes_to_midi(
     pm.instruments.append(instrument)
     pm.write(out_file)
     return pm
+
+def process_data(self, filenames,
+                      num_files = 5,
+                      vocab_size = 128,                     
+                      ):
+     """ preprocess the test data. Not made by me... """
+     
+    ## list for generated note
+    all_notes = []
+    n_notes = len(all_notes)
+    train_notes = np.stack([all_notes[key] for key in se     lf.key_order], axis=1)
+    notes_ds = tf.data.Dataset.from_tensor_slices(train_     notes)
+    notes_ds.element_spec
+
+    batch_size = 64
+    buffer_size = n_notes - self.seq_length  # the numbe     r of items in the dataset\
+    seq_ds = self._create_sequences(notes_ds, self.seq_l     ength, vocab_size)
+    seq_ds.element_spec
+
+    train_ds = (seq_ds
+                .shuffle(buffer_size)
+                .batch(batch_size, drop_remainder=True)
+                .cache()
+                .prefetch(tf.data.experimental.AUTOTUNE))
+ 
+         print(' --- Training Data Ready --- ')
+         return train_ds
+     
 
 if __name__ == "__main__":
     data_dir = '/Users/juan-garassino/Code/le-wagon/symphony/data/maestro-v2.0.0'
